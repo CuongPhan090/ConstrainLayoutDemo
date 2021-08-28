d82fb037b8852eb08096f255ff0bc2d49fba80e1
Cuong Phan <vcuong0909@gmail.com>
2021-08-28 18:11:41 -0400

    Init commit

---
+++ app/src/main/java/com/example/b32constraintlayoutdemo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.b32constraintlayoutdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+    }
+}
+
+//0x00000F87
+//0 15 135 100
